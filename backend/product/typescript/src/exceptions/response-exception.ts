e55dce9fd399fd1369c79172007d851936a639dd
jeruktutut2 <jeruktutut2@gmail.com>
2024-08-09 16:39:10 +0800

    remove product comments

--- backend/product/typescript/src/exceptions/response-exception.ts
+++
@@ -1,7 +0,0 @@
-// import { ErrorMessage } from "../helpers/error-message";
-
-// export class ResponseException extends Error {
-//     constructor(public status: number, public errorMessages: ErrorMessage[], public message: string) {
-//         super(message)
-//     }
-// }
--- backend/product/typescript/src/types/custom-response-type.ts
+++
@@ -1,5 +0,0 @@
-// import express, { Response } from 'express';
-
-// export interface CustomResponse extends Response {
-//     body?: any
-// }
